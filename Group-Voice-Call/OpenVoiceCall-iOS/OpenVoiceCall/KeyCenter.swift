7bffa2889a587348965964c35e02262cac33bde3
CavanSu <403029552@qq.com>
2020-02-27 17:39:17 +0800

    rtc static framework

--- Group-Voice-Call/OpenVoiceCall-iOS/OpenVoiceCall/KeyCenter.swift
+++ Group-Voice-Call/OpenVoiceCall-iOS/OpenVoiceCall/KeyCenter.swift
@@ -1,11 +1,14 @@
 //
 //  KeyCenter.swift
-//  OpenVoiceCall
+//  LargeGroupVideoChat
 //
-//  Created by GongYuhua on 5/16/16.
-//  Copyright © 2016 Agora. All rights reserved.
+//  Created by ZhangJi on 30/09/2017.
+//  Copyright © 2017 Agora. All rights reserved.
 //
 
 struct KeyCenter {
     static let AppId: String = <#Your App Id#>
+
+    // assign token to nil if you have not enabled app certificate
+    static var Token: String? = <#Temp Access Token#>
 }
--- One-to-One-Voice/Agora-iOS-Voice-Tutorial-Swift-1to1/Agora iOS Voice Tutorial/AppID.swift
+++ One-to-One-Voice/Agora-iOS-Voice-Tutorial-Swift-1to1/Agora iOS Voice Tutorial/AppID.swift
@@ -6,6 +6,6 @@
 //  Copyright © 2017年 Agora. All rights reserved.
 //
 
-let AppID: String = <#Your App ID#>
+let AppID: String = <#Your App Id#>
 // assign Token to nil if you have not enabled app certificate
-let Token: String? = <#Temp Token#>
+let Token: String? = <#Temp Access Token#>
