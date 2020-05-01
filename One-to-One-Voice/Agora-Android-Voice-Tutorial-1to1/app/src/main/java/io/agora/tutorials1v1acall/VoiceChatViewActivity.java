ca9a6572ef72fe86e94d7cf449b329fa43a18eed
Samyak Jain <samtan106@gmail.com>
2020-05-01 21:10:22 +0530

    ECO-94: Added Documentation for Basic Audio Call Android

--- One-to-One-Voice/Agora-Android-Voice-Tutorial-1to1/app/src/main/java/io/agora/tutorials1v1acall/VoiceChatViewActivity.java
+++ One-to-One-Voice/Agora-Android-Voice-Tutorial-1to1/app/src/main/java/io/agora/tutorials1v1acall/VoiceChatViewActivity.java
@@ -29,6 +29,23 @@ public class VoiceChatViewActivity extends AppCompatActivity {
     private RtcEngine mRtcEngine; // Tutorial Step 1
     private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() { // Tutorial Step 1
 
+        /**
+         * Occurs when a remote user (Communication)/host (Live Broadcast) leaves the channel.
+         *
+         * There are two reasons for users to become offline:
+         *
+         *     Leave the channel: When the user/host leaves the channel, the user/host sends a goodbye message. When this message is received, the SDK determines that the user/host leaves the channel.
+         *     Drop offline: When no data packet of the user or host is received for a certain period of time (20 seconds for the communication profile, and more for the live broadcast profile), the SDK assumes that the user/host drops offline. A poor network connection may lead to false detections, so we recommend using the Agora RTM SDK for reliable offline detection.
+         *
+         * @param uid ID of the user or host who
+         * leaves
+         * the channel or goes offline.
+         * @param reason Reason why the user goes offline:
+         *
+         *     USER_OFFLINE_QUIT(0): The user left the current channel.
+         *     USER_OFFLINE_DROPPED(1): The SDK timed out and the user dropped offline because no data packet was received within a certain period of time. If a user quits the call and the message is not passed to the SDK (due to an unreliable channel), the SDK assumes the user dropped offline.
+         *     USER_OFFLINE_BECOME_AUDIENCE(2): (Live broadcast only.) The client role switched from the host to the audience.
+         */
         @Override
         public void onUserOffline(final int uid, final int reason) { // Tutorial Step 4
             runOnUiThread(new Runnable() {
@@ -39,6 +56,16 @@ public class VoiceChatViewActivity extends AppCompatActivity {
             });
         }
 
+        /**
+         * Occurs when a remote user stops/resumes sending the audio stream.
+         * The SDK triggers this callback when the remote user stops or resumes sending the audio stream by calling the muteLocalAudioStream method.
+         *
+         * @param uid ID of the remote user.
+         * @param muted Whether the remote user's audio stream is muted/unmuted:
+         *
+         *     true: Muted.
+         *     false: Unmuted.
+         */
         @Override
         public void onUserMuteAudio(final int uid, final boolean muted) { // Tutorial Step 6
             runOnUiThread(new Runnable() {
@@ -127,6 +154,7 @@ public class VoiceChatViewActivity extends AppCompatActivity {
             iv.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
         }
 
+        // Stops/Resumes sending the local audio stream.
         mRtcEngine.muteLocalAudioStream(iv.isSelected());
     }
 
@@ -141,6 +169,9 @@ public class VoiceChatViewActivity extends AppCompatActivity {
             iv.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
         }
 
+        // Enables/Disables the audio playback route to the speakerphone.
+        //
+        // This method sets whether the audio is routed to the speakerphone or earpiece. After calling this method, the SDK returns the onAudioRouteChanged callback to indicate the changes.
         mRtcEngine.setEnableSpeakerphone(view.isSelected());
     }
 
@@ -153,6 +184,9 @@ public class VoiceChatViewActivity extends AppCompatActivity {
     private void initializeAgoraEngine() {
         try {
             mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+            // Sets the channel profile of the Agora RtcEngine.
+            // CHANNEL_PROFILE_COMMUNICATION(0): (Default) The Communication profile. Use this profile in one-on-one calls or group calls, where all users can talk freely.
+            // CHANNEL_PROFILE_LIVE_BROADCASTING(1): The Live-Broadcast profile. Users in a live-broadcast channel have a role as either broadcaster or audience. A broadcaster can both send and receive streams; an audience can only receive streams.
             mRtcEngine.setChannelProfile(Constants.CHANNEL_PROFILE_COMMUNICATION);
         } catch (Exception e) {
             Log.e(LOG_TAG, Log.getStackTraceString(e));
@@ -168,6 +202,7 @@ public class VoiceChatViewActivity extends AppCompatActivity {
             accessToken = null; // default, no token
         }
 
+        // Allows a user to join a channel.
         mRtcEngine.joinChannel(accessToken, "voiceDemoChannel1", "Extra Optional Data", 0); // if you do not specify the uid, we will generate the uid for you
     }
 
